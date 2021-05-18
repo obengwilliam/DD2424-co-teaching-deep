4c8aefef78913af2c25656abb6841a891d97b191
Weiqi Xu <vikixuzju@gmail.com>
2021-05-18 17:37:33 +0200

    Update Loss.py

--- Loss.py
+++ Loss.py
@@ -21,16 +21,16 @@ def loss_coteaching(y_1, y_2, t, R_rate):
     num_rem = int(len(t) * R_rate)
 
     # Calculate the loss of Network 1 and pick up the R_rate% smallest ones' index
-    loss_1 = F.cross_entropy(y_1, t, reduce=False) # Ignore the mean loss and return the loss of each element
+    loss_1 = F.cross_entropy(y_1, t, reduction='none') # Ignore the mean loss and return the loss of each element
     loss1_sorted_ind = bn.argpartition(loss_1.data, kth=num_rem)[:num_rem]  # .cuda()
 
     # Calculate the loss of Network 2 and rank
-    loss_2 = F.cross_entropy(y_2, t, reduce=False)
+    loss_2 = F.cross_entropy(y_2, t, reduction='none')
     loss2_sorted_ind = bn.argpartition(loss_2.data, kth=num_rem)[:num_rem]  # .cuda()
 
     # Exchange the index and use the other network's sample to calculate the loss
     loss_1_exchanged = F.cross_entropy(y_1[loss2_sorted_ind], t[loss2_sorted_ind])
-    loss_2_exchanged = F.cross_entropy(y_2[loss1_sorted_ind, t[loss1_sorted_ind]])
+    loss_2_exchanged = F.cross_entropy(y_2[loss1_sorted_ind], t[loss1_sorted_ind])
 
     # Return the exchanged loss for back propagation
     return torch.sum(loss_1_exchanged)/num_rem, torch.sum(loss_2_exchanged)/num_rem
